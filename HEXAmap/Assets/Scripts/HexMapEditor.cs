84b6562ce95999dba892a65b1dc680bbe15bb3cf
luca <tsar3701@korea.ac.kr>
2021-01-11 11:18:22 +0800

    맵 애디터 구현

--- HEXAmap/Assets/Scripts/HexMapEditor.cs
+++ HEXAmap/Assets/Scripts/HexMapEditor.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class HexMapEditor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public Color[] colors;
+	public HexGrid hexGrid;
+	private Color activeColor;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void Awake(){
+		SelectColor(0);
+	}
+	void Update(){
+		if(Input.GetMouseButton(0)){
+			HandleInput();
+		}
+	}
+
+	void HandleInput(){
+		Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if(Physics.Raycast(inputRay,out hit)){
+			hexGrid.ColorCell(hit.point, activeColor);
+		}
+	}
+
+	public void SelectColor(int index){
+		activeColor = colors[index];
+	}
 }
