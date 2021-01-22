3a8567d4561a6f48c1cca3883d641487b38a385d
LucaChen <44959242+tsarb0mba@users.noreply.github.com>
2021-01-22 16:22:05 +0800

    2-3-1 create method to create and clolor the quad

--- HEXAmap/Assets/Scripts/HexMesh.cs
+++ HEXAmap/Assets/Scripts/HexMesh.cs
@@ -74,4 +74,24 @@ public class HexMesh : MonoBehaviour {
 		colors.Add(cr2);
 		colors.Add(cr3);
 	}
+
+	//2-3-1 to blend triangulate region
+	void AddQuad(Vector3 v1, Vector3 v2, Vector3 v3){
+		int vertexIndex = vertices.Count;
+		vertices.Add(v1);
+		vertices.Add(v2);
+		vertices.Add(v3);
+		vertices.Add(v4);
+		triangles.Add(vertexIndex);
+		triangles.Add(vertexIndex+1);
+		triangles.Add(vertexIndex+2);
+		triangles.Add(vertexIndex+3);
+	}
+
+	void AddQuadColor(Color c1, Color c2, Color c3){
+		colors.Add(c1);
+		colors.Add(c2);
+		colors.Add(c3);
+		colors.Add(c4);
+	}
 }
